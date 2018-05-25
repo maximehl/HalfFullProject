11e9142715ca500704878886586bf5bebbc192ea
Nora Coates <noracoates@students.berkeley.net>
2018-05-25 10:33:36 -0700

    make button to continue litmus at will

--- nora.js
+++ nora.js
@@ -20,14 +20,15 @@ $(document).ready(function(){
             "Watch your favorite movie, with a friend or alone! Here's a <a href = 'https://www.doesthedogdie.com/'>helpful website</a> that tells you if something triggering happens.",
             "Volunteer at an animal shelter! Take care of something sweet and loving, and help people to boot.",
             "Play with Floofbunny!", "Sing along to your favorite song (or something that makes you pumped up) as loud as you can.",
-            "Practice a skill!", "Play a video game where you're the hero.", "Meditate using a guided meditation video on YouTube or a free app like Insight Timer.",
+            "Practice a skill you can do!", "Play a video game where you're the hero.", "Meditate using a guided meditation video on YouTube or a free app like Insight Timer.",
             "Go out for a run or do something fun and physical with a friend or family member.", "Think carefully and slowly through the things overwhelming you. " +
             "Break them down until they seem manageable--it might take some time but it's worth it!", "Try looking for a therapist in your area with some of the resources above. " +
             "Talking through your thoughts and feelings with someone trained to listen might help!", "Go and sit in the sun! Feel the wind on your face, lie in the grass. " +
             "Remember, we are all parts of a grand, beautiful network of life!", "Try adopting a new hobby or skill.", "Try something you haven't done in a while.",
             "Vent! Let out your feelings. Write them down, scream into your pillow, talk to your parents, tell your friends, tell your dog, sing them out, tell a therapist, tell a stranger...",
             "Eat something healthy! Physical health and mental health are closely related!", "Get clean--take a shower, or pamper yourself with a bath bomb.", "Paint your nails, dress up nicely... even if you don't go anywhere, looking nice feels nice!", "Make yourself some of your favorite food or drink, or go and get it--bring a friend, if you want!",
-            "Come out of your room. Even if you don't talk to your parents outright about what you're feeling, being with others often helps. Alone time is good, but company is too!", "Stick your head out the window and listen to the world around you. You're a big, important part of this big, important world!"
+            "Come out of your room. Even if you don't talk to your parents outright about what you're feeling, being with others often helps. Alone time is good, but company is too!",
+            "Stick your head out the window and listen to the world around you. You're a big, important part of this big, important world!", "Slow down and take a deep breath. Where you are is okay--you're okay."
         ];
         //feeds a random suggestion from the list into the div
         $(this).html(list[Math.floor(Math.random() * list.length)]);
@@ -45,11 +46,13 @@ $(document).ready(function(){
 
     $("#dia").on("click", function() {
         var slides = $("#slidecontainer");
+
         if($(this).hasClass("show")) {
             slides.css({"opacity": "100"});
-            $(this).parent.click();
+
             $(this).removeClass("show");
             slides.click();
+            $("#diagnosis").click();
         } else {
             slides.css({"opacity": "0"});
             $(this).addClass("show");
@@ -120,22 +123,25 @@ function getHeightNeeded(toggleObj){
 
 function diagnoseTest(array) {
     //assigns a weight to each of them--being suicidal is more worrying than insomnia
-    var percentWeights = [.7, .8, .8, 1.3, .8, 1.5, 1.4, .7, 2];
+    var percentWeights = [.07, .08, .08, .13, .08, .15, .14, .07, .2];
     var total = 0;
 
+
+    //add each array value multiplied by its weight to the total (should be 100 if all full)
+    for (var i = 0; i < 9; i++) {
+        console.log(total);
+        total += array[i] * percentWeights[i]
+    }
     //if the value of the 'suicidal' or 'worthless' bar is too high, emergency mode
     if (array[8] >= 6 || array[6] > 8) {
         total += 60;
     }
-    //add each array value multiplied by its weight to the total (should be 100 if all full)
-    for (var i = 0; i < 9; i++) {
-        total += array[i] * percentWeights[i]
-    }
+    console.log(total);
 
     if (total >= 80) {
         $("#special").html("Even if you aren't diagnosed with depression, you should talk to someone about how you've been feeling. This isn't a healthy mindspace, and if you get help, you can get out of it." +
             " Take a look through the hotlines and warmlines available at the top left, and try having a talk with someone you trust or" +
-            " a trained professional about how you feel. Please remember that we care for you and you are important and worth far more than you might think.");
+            " a trained professional about how you feel. Please remember that we care for you! You are important and worth far more than you might think.");
         //get help
     } else if (total >= 40) {
         $("#special").html("Even if you aren't diagnosed with depression, it looks like you need to get yourself out of the mental space you're in right now." +
