9ab5fc712eac460e3389e939057ca42206da84b8
Nora Coates <31967440+NoraWC@users.noreply.github.com>
2018-05-28 15:47:19 -0700

    Update nora.js

--- nora.js
+++ nora.js
@@ -43,7 +43,7 @@ $(document).ready(function(){
             event.stopPropagation();
         });
         $(this).on("mouseup", function() {
-            CURRENT_SLIDE = $(this).id;
+            CURRENT_SLIDE = $(this);
         });
     });
     
